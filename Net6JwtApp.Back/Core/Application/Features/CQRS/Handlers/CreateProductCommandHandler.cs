8ca4955e5d0e50e8ad315fc835bc7b6c8a5becd9
hanoguzakpinar <oguzhanakpinar@yahoo.com>
2022-07-11 15:57:38 +0300

    createproductcommand

---
+++ Net6JwtApp.Back/Core/Application/Features/CQRS/Handlers/CreateProductCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Net6JwtApp.Back.Core.Application.Features.CQRS.Commands;
+using Net6JwtApp.Back.Core.Application.Interfaces;
+using Net6JwtApp.Back.Core.Domain;
+
+namespace Net6JwtApp.Back.Core.Application.Features.CQRS.Handlers
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest>
+    {
+        private readonly IRepository<Product> _repository;
+
+        public CreateProductCommandHandler(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            await _repository.CreateAsync(new Product()
+            {
+                CategoryId = request.CategoryId,
+                Name = request.Name,
+                Price = request.Price,
+                Stock = request.Stock
+            });
+            return Unit.Value;
+        }
+    }
+}
